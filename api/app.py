29c09e3a86e58d49713535d5e65ecba811b6a8ff
mihai-ciorobitca <mihai.ciorobitca@outlook.com>
2024-07-24 20:31:51 +0300

    m

--- api/app.py
+++ api/app.py
@@ -21,6 +21,10 @@ cache = Cache(app)
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 @app.route('/login')
 def login():
     return render_template('login.html')
